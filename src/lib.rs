3c518d42ab4d1e30be5699eee9193e390b023c38
Master-Hash <hash@hash.memorial>
2025-02-21 14:04:24 +0100

    refactor: Rust 2024 & use CStr literal

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,16 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 use icu_segmenter::WordSegmenter;
 #[cfg(all(not(feature = "windows"), feature = "icu_segmenter"))]
 use itertools::Itertools;
-use std::ffi::CString;
 use std::os::raw;
 use std::ptr;
 #[cfg(feature = "windows")]
 use std::sync::LazyLock;
 #[cfg(feature = "windows")]
-use windows::core::h;
+use windows::Data::Text::SelectableWordsSegmenter;
 #[cfg(feature = "windows")]
 use windows::core::HSTRING;
 #[cfg(feature = "windows")]
-use windows::Data::Text::SelectableWordsSegmenter;
+use windows::core::h;
 
 #[cfg(feature = "windows")]
 static segmenter: LazyLock<SelectableWordsSegmenter> =
@@ -27,61 +26,60 @@ static segmenter: LazyLock<SelectableWordsSegmenter> =
 // icu segmenter seems unable to be initialized in a static variable
 // Rc is used inside, so it is not Sync
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 #[allow(non_upper_case_globals)]
 pub static plugin_is_GPL_compatible: libc::c_int = 1;
 
 // ordinarily the Rust compiler will mangle funciton names. we don't want to do
 // that, since the C code won't know what code to call.
 // we'll also want to use `unsafe` because we need access to raw pointers
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn emacs_module_init(runtime: *mut emacs_runtime) -> libc::c_int {
-    let fset_string = CString::new("fset").unwrap();
+    unsafe {
+        let env = (*runtime).get_environment.unwrap()(runtime);
 
-    let env = (*runtime).get_environment.unwrap()(runtime);
+        let intern = (*env).intern.unwrap();
+        let funcall = (*env).funcall.unwrap();
+        let make_function = (*env).make_function.unwrap();
 
-    let intern = (*env).intern.unwrap();
-    let funcall = (*env).funcall.unwrap();
-    let make_function = (*env).make_function.unwrap();
+        let Qfset = intern(env, c"fset".as_ptr());
 
-    let Qfset = intern(env, fset_string.as_ptr());
-
-    let func_emt__do_split_helper_description = CString::new("This function takes a string and return an array of bounds. A bound is a cons with the starting position and the ending position of a word.").unwrap();
-    let func_emt__word_at_point_or_forward_description = CString::new("This functions takes a string and a position, and returns the bound of the word at the position. If the position is at bound of two words, it returns the word at the right side of that position. This function does not tokenize the whole string, so it is faster in some cases.").unwrap();
-    let func_emt__do_split_helper_name = CString::new("emt--do-split-helper").unwrap();
-    let func_emt__word_at_point_or_forward_name =
-        CString::new("emt--word-at-point-or-forward-helper").unwrap();
-    let Qsplit_helper = intern(env, func_emt__do_split_helper_name.as_ptr());
-    let Qword_at_point = intern(env, func_emt__word_at_point_or_forward_name.as_ptr());
-    let func_emt__do_split_helper = make_function(
-        env,
-        1,
-        1,
-        Some(Femt__do_split_helper),
-        func_emt__do_split_helper_description.as_ptr(),
-        std::ptr::null_mut(),
-    );
-    funcall(
-        env,
-        Qfset,
-        2,
-        [Qsplit_helper, func_emt__do_split_helper].as_mut_ptr(),
-    );
-    let func_emt__do_split_helper = make_function(
-        env,
-        2,
-        2,
-        Some(Femt__word_at_point_or_forward),
-        func_emt__word_at_point_or_forward_description.as_ptr(),
-        std::ptr::null_mut(),
-    );
-    funcall(
-        env,
-        Qfset,
-        2,
-        [Qword_at_point, func_emt__do_split_helper].as_mut_ptr(),
-    );
-    0
+        let func_emt__do_split_helper_description = c"This function takes a string and return an array of bounds. A bound is a cons with the starting position and the ending position of a word.";
+        let func_emt__word_at_point_or_forward_description = c"This functions takes a string and a position, and returns the bound of the word at the position. If the position is at bound of two words, it returns the word at the right side of that position. This function does not tokenize the whole string, so it is faster in some cases.";
+        let func_emt__do_split_helper_name = c"emt--do-split-helper";
+        let func_emt__word_at_point_or_forward_name = c"emt--word-at-point-or-forward-helper";
+        let Qsplit_helper = intern(env, func_emt__do_split_helper_name.as_ptr());
+        let Qword_at_point = intern(env, func_emt__word_at_point_or_forward_name.as_ptr());
+        let func_emt__do_split_helper = make_function(
+            env,
+            1,
+            1,
+            Some(Femt__do_split_helper),
+            func_emt__do_split_helper_description.as_ptr(),
+            std::ptr::null_mut(),
+        );
+        funcall(
+            env,
+            Qfset,
+            2,
+            [Qsplit_helper, func_emt__do_split_helper].as_mut_ptr(),
+        );
+        let func_emt__do_split_helper = make_function(
+            env,
+            2,
+            2,
+            Some(Femt__word_at_point_or_forward),
+            func_emt__word_at_point_or_forward_description.as_ptr(),
+            std::ptr::null_mut(),
+        );
+        funcall(
+            env,
+            Qfset,
+            2,
+            [Qword_at_point, func_emt__do_split_helper].as_mut_ptr(),
+        );
+        0
+    }
 }
 
 unsafe extern "C" fn Femt__do_split_helper(
@@ -90,64 +88,65 @@ unsafe extern "C" fn Femt__do_split_helper(
     args: *mut emacs_value,
     data: *mut raw::c_void,
 ) -> emacs_value {
-    let cons_string = CString::new("cons").unwrap();
-    let vector_string = CString::new("vector").unwrap();
-    let intern = (*env).intern.unwrap();
-    let funcall = (*env).funcall.unwrap();
-    let make_integer = (*env).make_integer.unwrap();
-    let copy_string_contents = (*env).copy_string_contents.unwrap();
+    unsafe {
+        let intern = (*env).intern.unwrap();
+        let funcall = (*env).funcall.unwrap();
+        let make_integer = (*env).make_integer.unwrap();
+        let copy_string_contents = (*env).copy_string_contents.unwrap();
 
-    let Qcons = intern(env, cons_string.as_ptr());
-    let Qvector = intern(env, vector_string.as_ptr());
+        let Qcons = intern(env, c"cons".as_ptr());
+        let Qvector = intern(env, c"vector".as_ptr());
 
-    let mut len: isize = 0;
-    let is_ok = copy_string_contents(env, *args, ptr::null_mut(), &mut len);
-    let mut buf = vec![0u8; len as usize];
-    let is_ok = copy_string_contents(env, *args, buf.as_mut_ptr() as *mut raw::c_char, &mut len);
+        let mut len: isize = 0;
+        let is_ok = copy_string_contents(env, *args, ptr::null_mut(), &mut len);
+        let mut buf = vec![0u8; len as usize];
+        let is_ok =
+            copy_string_contents(env, *args, buf.as_mut_ptr() as *mut raw::c_char, &mut len);
 
-    strip_trailing_zero_bytes(&mut buf);
+        strip_trailing_zero_bytes(&mut buf);
 
-    let param_u8 = String::from_utf8(buf).unwrap();
-    #[cfg(feature = "windows")]
-    let mut consCell = {
-        let param_hstring = HSTRING::from(param_u8);
-        let res = segmenter.GetTokens(&param_hstring).unwrap();
+        let param_u8 = String::from_utf8(buf).unwrap();
+        #[cfg(feature = "windows")]
+        let mut consCell = {
+            let param_hstring = HSTRING::from(param_u8);
+            let res = segmenter.GetTokens(&param_hstring).unwrap();
 
-        let iConsCell = res.into_iter().map(|i| {
-            let segment = i.SourceTextSegment().unwrap();
-            let l = make_integer(env, segment.StartPosition.into());
-            let r = make_integer(env, (segment.StartPosition + segment.Length).into());
-            funcall(env, Qcons, 2, [l, r].as_mut_ptr())
-        });
-        iConsCell.collect::<Vec<_>>()
-    };
-    #[cfg(all(not(feature = "windows"), feature = "icu_segmenter"))]
-    let mut consCell = {
-        let segmenter_icu = WordSegmenter::new_auto();
-        let segments = segmenter_icu
-            .segment_str(&param_u8)
-            .tuple_windows()
-            .map(|(i, j)| &param_u8[i..j]);
-        let ss = segments.map(|s| s.chars().count());
-        // we need prefix sum
-        // from: [4, 1, 4]
-        // to [(0, 4), (4, 5), (5, 9)]
-        let iConsCell = ss
-            .scan(0, |acc, x| {
-                let res = Some((*acc, *acc + x));
-                *acc += x;
-                res
-            })
-            .map(|(l, r)| {
-                let l = make_integer(env, l as i64);
-                let r = make_integer(env, r as i64);
+            let iConsCell = res.into_iter().map(|i| {
+                let segment = i.SourceTextSegment().unwrap();
+                let l = make_integer(env, segment.StartPosition.into());
+                let r = make_integer(env, (segment.StartPosition + segment.Length).into());
                 funcall(env, Qcons, 2, [l, r].as_mut_ptr())
             });
-        iConsCell.collect::<Vec<_>>()
-    };
-    let l = consCell.len();
-    let ddd = consCell.as_mut_ptr();
-    funcall(env, Qvector, l as isize, ddd)
+            iConsCell.collect::<Vec<_>>()
+        };
+        #[cfg(all(not(feature = "windows"), feature = "icu_segmenter"))]
+        let mut consCell = {
+            let segmenter_icu = WordSegmenter::new_auto();
+            let segments = segmenter_icu
+                .segment_str(&param_u8)
+                .tuple_windows()
+                .map(|(i, j)| &param_u8[i..j]);
+            let ss = segments.map(|s| s.chars().count());
+            // we need prefix sum
+            // from: [4, 1, 4]
+            // to [(0, 4), (4, 5), (5, 9)]
+            let iConsCell = ss
+                .scan(0, |acc, x| {
+                    let res = Some((*acc, *acc + x));
+                    *acc += x;
+                    res
+                })
+                .map(|(l, r)| {
+                    let l = make_integer(env, l as i64);
+                    let r = make_integer(env, r as i64);
+                    funcall(env, Qcons, 2, [l, r].as_mut_ptr())
+                });
+            iConsCell.collect::<Vec<_>>()
+        };
+        let l = consCell.len();
+        let ddd = consCell.as_mut_ptr();
+        funcall(env, Qvector, l as isize, ddd)
+    }
 }
 
 unsafe extern "C" fn Femt__word_at_point_or_forward(
@@ -156,71 +155,73 @@ unsafe extern "C" fn Femt__word_at_point_or_forward(
     args: *mut emacs_value,
     data: *mut raw::c_void,
 ) -> emacs_value {
-    let cons_string = CString::new("cons").unwrap();
-    let intern = (*env).intern.unwrap();
-    let funcall = (*env).funcall.unwrap();
-    let make_integer = (*env).make_integer.unwrap();
-    let extract_integer = (*env).extract_integer.unwrap();
-    let copy_string_contents = (*env).copy_string_contents.unwrap();
+    unsafe {
+        let intern = (*env).intern.unwrap();
+        let funcall = (*env).funcall.unwrap();
+        let make_integer = (*env).make_integer.unwrap();
+        let extract_integer = (*env).extract_integer.unwrap();
+        let copy_string_contents = (*env).copy_string_contents.unwrap();
 
-    let Qcons = intern(env, cons_string.as_ptr());
+        let Qcons = intern(env, c"cons".as_ptr());
 
-    let mut len: isize = 0;
-    let is_ok = copy_string_contents(env, *args, ptr::null_mut(), &mut len);
-    let mut buf = vec![0u8; len as usize];
-    let is_ok = copy_string_contents(env, *args, buf.as_mut_ptr() as *mut raw::c_char, &mut len);
+        let mut len: isize = 0;
+        let is_ok = copy_string_contents(env, *args, ptr::null_mut(), &mut len);
+        let mut buf = vec![0u8; len as usize];
+        let is_ok =
+            copy_string_contents(env, *args, buf.as_mut_ptr() as *mut raw::c_char, &mut len);
 
-    strip_trailing_zero_bytes(&mut buf);
+        strip_trailing_zero_bytes(&mut buf);
 
-    let n = extract_integer(env, *args.offset(1));
+        let n = extract_integer(env, *args.offset(1));
 
-    let param_u8 = String::from_utf8(buf).unwrap();
-    #[cfg(feature = "windows")]
-    let (l, r) = {
-        let param_hstring = HSTRING::from(param_u8);
-        let res = segmenter
-            .GetTokenAt(&param_hstring, n.try_into().unwrap())
-            .unwrap();
+        let param_u8 = String::from_utf8(buf).unwrap();
+        #[cfg(feature = "windows")]
+        let (l, r) = {
+            let param_hstring = HSTRING::from(param_u8);
+            let res = segmenter
+                .GetTokenAt(&param_hstring, n.try_into().unwrap())
+                .unwrap();
 
-        let segment = res.SourceTextSegment().unwrap();
-        let l = make_integer(env, segment.StartPosition.into());
-        let r = make_integer(env, (segment.StartPosition + segment.Length).into());
-        (l, r)
-    };
-    #[cfg(all(not(feature = "windows"), feature = "icu_segmenter"))]
-    let (l, r) = {
-        // Sadly WordSegmenter does not provide a way to get the nth token
-        let segmenter_icu = WordSegmenter::new_auto();
-        let segments = segmenter_icu
-            .segment_str(&param_u8)
-            .tuple_windows()
-            .map(|(i, j)| &param_u8[i..j]);
-        let mut ss = segments.map(|s| s.chars().count());
-        // we need prefix sum
-        // from: [4, 1, 4], 4
-        // to [(4, 5)]
-        // from: [4, 1, 4], 6
-        // to [(5, 9)]
-        let iConsCell = ss.try_fold(0, |acc, x| {
-            let r = acc + x;
-            let l = acc;
-            if n < r.try_into().unwrap() {
-                Err((l, r))
-            } else {
-                Ok(r)
+            let segment = res.SourceTextSegment().unwrap();
+            let l = make_integer(env, segment.StartPosition.into());
+            let r = make_integer(env, (segment.StartPosition + segment.Length).into());
+            (l, r)
+        };
+        #[cfg(all(not(feature = "windows"), feature = "icu_segmenter"))]
+        let (l, r) = {
+            // Sadly WordSegmenter does not provide a way to get the nth token
+            let segmenter_icu = WordSegmenter::new_auto();
+            let segments = segmenter_icu
+                .segment_str(&param_u8)
+                .tuple_windows()
+                .map(|(i, j)| &param_u8[i..j]);
+            let mut ss = segments.map(|s| s.chars().count());
+            // we need prefix sum
+            // from: [4, 1, 4], 4
+            // to [(4, 5)]
+            // from: [4, 1, 4], 6
+            // to [(5, 9)]
+            let iConsCell = ss.try_fold(0, |acc, x| {
+                let r = acc + x;
+                let l = acc;
+                if n < r.try_into().unwrap() {
+                    Err((l, r))
+                } else {
+                    Ok(r)
+                }
+            });
+            match iConsCell {
+                // Seems all the program will be panic if we reach here
+                Ok(_) => unreachable!(),
+                Err((l, r)) => {
+                    let l = make_integer(env, l as i64);
+                    let r = make_integer(env, r as i64);
+                    (l, r)
+                }
             }
-        });
-        match iConsCell {
-            // Seems all the program will be panic if we reach here
-            Ok(_) => unreachable!(),
-            Err((l, r)) => {
-                let l = make_integer(env, l as i64);
-                let r = make_integer(env, r as i64);
-                (l, r)
-            }
-        }
-    };
-    funcall(env, Qcons, 2, [l, r].as_mut_ptr())
+        };
+        funcall(env, Qcons, 2, [l, r].as_mut_ptr())
+    }
 }
 
 // Thank you,
